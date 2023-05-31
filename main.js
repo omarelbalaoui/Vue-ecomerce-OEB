358fc26daeaa38809d78b4f8b1c4187cb558bb89
omarelbalaoui <omarelbalaoui@iesebre.com>
2023-05-31 22:17:33 +0200

    L7

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart:0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -16,13 +17,6 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        // solution
-        removeFromCart() {
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
-        },
-        // solution
         updateImage(variantImage) {
             this.image = variantImage
         }
